d054871bd01a46656d6f85fd6fd21bef07d805d7
Amber Scott <amber.sorenson@gmail.com>
2019-06-21 15:27:32 -0600

    Helper_Functions.py is working with bed but tabix hates is

--- Helper_Functions.py
+++ Helper_Functions.py
@@ -13,7 +13,7 @@ import tempfile
 # Output: number of significant SNPs
 
 
-def get_intervals(GWAS_infile, Interval_outfile,  pval_thresh, window_size):
+def get_intervals(GWAS_infile, Interval_outfile, gNOMAD_outfile,  pval_thresh, window_size, gNOMAD_db):
 
 	# Args
 	# GWAS infile is a tab delimited text file
@@ -38,29 +38,38 @@ def get_intervals(GWAS_infile, Interval_outfile,  pval_thresh, window_size):
 	window_interval = int(int(window_size)/2)
 	bedfile_tmp = tempfile.NamedTemporaryFile(mode='w')
 	for marker in GWAS_sig:
-		chrom = 'chr' + str(marker[0])
+		chrom = str(marker[0])
 		start_pos = str(marker[1]-window_interval)
 		stop_pos = str(marker[1]+window_interval)
 		name = marker[2]
 		score = str(marker[3])
-		line = [chrom,start_pos,stop_pos,name,score]
+		line = [chrom,start_pos,stop_pos,name+','+score]
 		newline = '\t'.join(line)+'\n'
-		print(newline)
 		bedfile_tmp.write(newline)
-	interval_file=open(Interval_outfile,'w')	
-	merged_intervals=subprocess.run(["bedtools","merge", "-i",bedfile_tmp.name], capture_output=True)
+	# interval_file=tempfile.NamedTemporaryFile(mode='w')	
+	interval_file=open(Interval_outfile,'w')
+	merged_intervals=subprocess.run(["bedtools","merge", "-i",bedfile_tmp.name, "-c", "4","-o","collapse","-delim",";"], capture_output=True)
 	output = merged_intervals.stdout
 	interval_file.write(output.decode('utf-8'))
 	interval_file.close()
+	
+	gNOMAD_outfile=open(gNOMAD_outfile,'w')
+	gNOMAD_call=subprocess.run(["tabix",gNOMAD_db,"-B",Interval_outfile],capture_output=True)
+	gNOMAD_stdout=gNOMAD_call.stdout
+	gNOMAD_outfile.write(gNOMAD_stdout.decode('utf-8'))
+	gNOMAD_outfile.close()
+	
 
 def main():
 	infile = sys.argv[1]
 	outfile = sys.argv[2]
+	gNOMAD_outfile =sys.argv[3]
 	#pval = sys.argv[3]
 	#window = sys.argv[4]
-	pval_default=float(5e-8)
+	pval_default=float(5e-12)
 	window = 10000
-	get_intervals(infile, outfile,  pval_default, window)
+	gNOMAD_db='/filestore/gnomad/gnomad.genomes.r2.1.1.sites.vcf.bgz'
+	get_intervals(infile, outfile, gNOMAD_outfile, pval_default, window, gNOMAD_db)
 
 
 if __name__ == '__main__':
